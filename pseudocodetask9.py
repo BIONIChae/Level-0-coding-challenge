b61fe4d7f5a263fc182f429a71a209a67afffcb8
Chantel Dhlamini <chanteldhlamini12@gmail.com>
2021-09-21 01:15:45 +0200

    nineth commit

---
+++ pseudocodetask9.py
@@ -0,0 +1,13 @@
+str = input()
+print("Vowels:")
+for x in str:
+    if x == 'a':
+        print('a')
+    if x == 'e':
+        print('e')
+    if (x == 'i'):
+        print('i')
+    if (x == 'o'):
+        print('o')
+    if (x == 'u'):
+        print('u')
