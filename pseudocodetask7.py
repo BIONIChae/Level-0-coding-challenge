162dd8ac1a22628f70c6639b7fb1dac0a342b64e
Chantel Dhlamini <chanteldhlamini12@gmail.com>
2021-09-21 01:07:15 +0200

    seventh commit

---
+++ pseudocodetask7.py
@@ -0,0 +1,13 @@
+celsius = int(input())
+c = (9/5 * celsius) + 32
+def convert(c):
+    return c
+fahrenheit = c
+print(fahrenheit)
+
+fahrenheit = int(input())
+f = (fahrenheit - 32) * 5/9
+def convert(f):
+    return f
+celsius = f
+print(celsius)
