71d14c55160b19694a8d03ea0a963930f059ee4b
Chantel Dhlamini <chanteldhlamini12@gmail.com>
2021-09-22 12:53:05 +0200

    fifth commit

--- pseudocodetask5.py
+++ pseudocodetask5.py
@@ -1,4 +1,6 @@
-base = int(input())
-height = int(input())
-area_of_a_triangle = (base * height) / 2
-print(area_of_a_triangle)
+def sides_of_triangle(x, y, z):
+    semi_perimeter = (x + y + z) / 2
+    s = semi_perimeter
+    area = (s * (s-x) * (s-y) * (s-z)) ** 0.5
+    print(area)
+sides_of_triangle()
