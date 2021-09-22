44165ec335bb54975a3be87f491337cbcf4e96b7
Chantel Dhlamini <chanteldhlamini12@gmail.com>
2021-09-22 13:15:59 +0200

    eighth commit

--- pseudocodetask8.py
+++ pseudocodetask8.py
@@ -1,4 +1,5 @@
-time = int(input())
-hour = int(time / 60)
-minute = time % 60
-print( hour, "hour(s),", minute, "minute(s)")
+def convert(number):
+    hour = int(number/ 60)
+    minute = int(number % 60)
+    print(hour, "hour(s),", minute, "minute(s)")
+convert()
