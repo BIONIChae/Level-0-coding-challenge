a2e7e7a617bc9cbf047d20c067ca39d3daefec70
Chantel Dhlamini <chanteldhlamini12@gmail.com>
2021-09-29 17:12:21 +0200

    tenth commit

--- pseudocodetask10.py
+++ pseudocodetask10.py
@@ -1,6 +1,12 @@
 def common(string1, string2):
-    common_values = list(set(string1) & set(string2))
-    print("Common letters: ")
-    for items in common_values:
-        print(items)
-common()
+  string1Set = set()
+  for letter in string1.lower():
+    string1Set.add(letter)
+
+  string2Set = set()
+  for letter in string2.lower():
+    string2Set.add(letter)
+
+  common_words = list(string1Set.intersection(string2Set))
+  print('Common letters: ' + ','.join(common_words))
+common('house', 'computers')
