937ce55d2ea9a673f8bc42badb21c0acfe4e7910
Chantel Dhlamini <chanteldhlamini12@gmail.com>
2021-09-29 16:00:43 +0200

    nineth commit

--- pseudocodetask9.py
+++ pseudocodetask9.py
@@ -1,14 +1,10 @@
-def string(word):
-  for letter in word:
-    if letter == 'a' or letter == 'A':
-        print('a')
-    if letter == 'e' or letter == 'E':
-        print('e')
-    if letter == 'i' or letter == 'I':
-        print('i')
-    if letter == 'o' or letter == 'O':
-        print('o')
-    if letter == 'u' or letter == 'U':
-        print('u')
-print("Vowels:")
-string()
+ef print_vowel(word):
+    vowels = ['a', 'e', 'i', 'o', 'u']
+    output = "" 
+   
+    for letter in word.lower():
+        if letter in vowels:
+            output += letter + ""
+    print("Vowels:", ', '.join(dict.fromkeys(output)))
+print_vowel("Umuzi")
+
