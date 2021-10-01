3936e4f1f45c8506cfe2b96eed22551c40f68fe6
BIONIChae <89851362+BIONIChae@users.noreply.github.com>
2021-10-01 10:57:36 +0200

    Update pseudocodetask7.py

--- pseudocodetask7.py
+++ pseudocodetask7.py
@@ -1,10 +1,10 @@
 def convert(celsius):
     fahrenheit = (9/5 * celsius) + 32
     print(fahrenheit)
-convert()
+convert(30)
 
 def convert(fahrenheit):
     celsius = (fahrenheit - 32) * 5/9
     print(celsius)
-convert()
+convert(87)
 
